2cf12327a004464ae32d3459601ed4dfe89548f9
Fedora <Fedora>
2026-05-12 16:20:48 -0300

    Aprendizado extremamente básico de ponteiros e alocação dinâmica de memória

--- c/alocacao_memoria.c
+++ c/alocacao_memoria.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <stdio.h> // Entrada e saída (I/O)
+#include <stdlib.h> // Alocação dinâmica de memória
 
 // Funções (superficialmente)
 
@@ -55,11 +56,66 @@ int main(void) {
 
     int x = 5;
     int *ptr = &x;
-    printf("Valor do ponteiro apontando pra x: %d\n", x);
+    int **ptrr = &ptr;
+    printf("Valor de x: %d\n", x);
+    printf("Valor do ponteiro apontando pra x: %d\n", *ptr);
+    printf("Valor de um ponteiro apontando pra outro ponteiro: %d\n", **ptrr);
     
     // Alocação dinâmica de memória (principal[malloc, calloc, realloc, free])
 
-    // ...
+    // Malloc (não limpa memória)
+
+    int *ponteiro = (int *)malloc(sizeof(int)  * 10);
+
+    if (ponteiro == NULL) {
+        printf("Erro na alocação\n");
+    }
+
+    ponteiro[0] = 10;
+    printf("Ponteiro inteiro com memória não limpa: %d\n", ponteiro[0]);
+    free(ponteiro);
+
+    // Calloc (limpa memória)
+
+    int *ponteiro0 = (int *)calloc(10, sizeof(int));
+
+    if (ponteiro0 == NULL) {
+        printf("Erro na alocação\n");
+    }
+
+    ponteiro0[0] = 10;
+    printf("Ponteiro inteiro com memória limpa: %d\n", ponteiro[0]);
+    free(ponteiro0);
+
+    // Realloc e Malloc
+
+    int *ponteiro1 = (int *)malloc(sizeof(int) * 10);
+
+    if (ponteiro1 == NULL) {
+        printf("Erro na alocação\n");
+    }
+
+    ponteiro1[0] = 10;
+
+    ponteiro1 = (int *)realloc(ponteiro1, sizeof(int) * 5);
+
+    printf("Ponteiro inteiro com alocação dinâmica de memória com malloc realocado com realloc: %d\n", ponteiro1[0]);
+    free(ponteiro1);
+
+    // Realloc e Calloc
+
+    int *ponteiro2 = (int *)calloc(5, sizeof(int));
+
+    if (ponteiro2 == NULL) {
+        printf("Erro na alocação\n");
+    }
+
+    ponteiro2[0] = 10;
+
+    ponteiro2 = (int *)realloc(ponteiro2, sizeof(int) * 10);
+
+    printf("Ponteiro inteiro com alocação dinâmica de memória com calloc realocado com realloc: %d\n", ponteiro2[0]);
+    free(ponteiro2);
     
     return 0;
 }
